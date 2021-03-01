b4282268a2f8b686ff13ae529c66ae998a4d205c
Mockinbrd <marvyn.ap@gmail.com>
2021-03-01 15:25:48 +0100

    :wrench: : tree;

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="App-header">
+        <h1>
+          <a href="/">react-blind-test</a>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
