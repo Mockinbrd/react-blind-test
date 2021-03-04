c242928e7d8a1e679b1179aeca0927fd2ef1fc33
m.aboulicam <m.aboulicam@eenov.com>
2021-03-04 15:53:22 +0100

    :sparkles: : babel import; register component;

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import tw, { styled } from "twin.macro";
+import PrimaryButton from "../../styles/primaryButton";
 
-const textInput = styled.input`
-  ${tw`focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md`}
+const TextInput = styled.input`
+  ${tw`focus:ring-cyan-600 focus:border-cyan-600 block w-full pl-10 sm:text-sm border-gray-300 rounded-md`}
 `;
-const twLabel = styled.label`
-  ${tw`block text-sm font-medium text-gray-700`}
+const TwLabel = styled.label`
+  ${tw`block text-sm font-medium text-gray-600 mt-6`}
 `;
-const parentInputDiv = styled.div`
+const ParentInputDiv = styled.div`
   ${tw`mt-1 relative rounded-md shadow-sm`}
 `;
-const parentSvgDiv = styled.div`
+const ParentSvgDiv = styled.div`
   ${tw`absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none`}
 `;
 
@@ -18,29 +19,53 @@ const Register = () => {
   return (
     <div className="flex flex-grow">
       <div className="m-auto">
-        <div className="grid grid-cols-1 gap-6">
-          <h1>Register</h1>
-          <form>
-            <twLabel htmlFor="email">Email</twLabel>
-            <parentInputDiv>
-              <parentSvgDiv>
-                <svg
-                  className="h-5 w-5 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+        <div className="grid grid-cols-1 gap-4 p-8">
+          <h1 className="text-center text-3xl text-gray-500 font-delius">Register</h1>
+          <form className="w-80">
+            <TwLabel htmlFor="email">Email</TwLabel>
+            <ParentInputDiv>
+              <ParentSvgDiv>
+                <svg tw="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                 </svg>
-              </parentSvgDiv>
-              <textInput
+              </ParentSvgDiv>
+              <TextInput
                 type="email"
                 id="email"
                 placeholder="you@example.com"
-              ></textInput>
-            </parentInputDiv>
+              ></TextInput>
+            </ParentInputDiv>
+            <TwLabel htmlFor="password">Password</TwLabel>
+            <ParentInputDiv>
+              <ParentSvgDiv>
+                <svg tw="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+                </svg>
+              </ParentSvgDiv>
+              <TextInput
+                type="password"
+                id="password"
+                placeholder="•••••••••"
+              ></TextInput>
+            </ParentInputDiv>
+            <TwLabel htmlFor="passwordConfirmation">Password confirmation</TwLabel>
+            <ParentInputDiv>
+              <ParentSvgDiv>
+                <svg tw="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+                </svg>
+              </ParentSvgDiv>
+              <TextInput
+                type="password"
+                id="passwordConfirmation"
+                placeholder="•••••••••"
+              ></TextInput>
+            </ParentInputDiv>
+            <div className="flex justify-center mt-8">
+              <PrimaryButton type="submit">
+                Sign Up
+              </PrimaryButton>
+            </div>
           </form>
         </div>
       </div>
